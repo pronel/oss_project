7a1f2910a3a8383b5ff7663205d41e3fe5069d40
pronel <yc9870@naver.com>
2016-11-15 14:05:02 +0900

    Update rank.h
    동적할당을 하고 맵을 참조해서 스테이지 갯수를 얻은뒤 스테이지별로 출력을 할수있게 틀을 수정함

--- rank.h
+++ rank.h
@@ -1,28 +1,65 @@
 #include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include<stdlib.h>  //system(x)를 사용하기 위해 선언
+#include"map.h"  // 맵 스테이지 갯수(Maxstage)를 셈하기 위해서 참조
+
 
 typedef struct rank{
+	int stage;
 	char name[4];
 	int move_count;
-	rank *next;
+	rank next*;
 }RANK;
 
 void print_rank(){
 	
 	FILE *f1;
-	rank *ranking;
-	int i = 0;
+	rank *list;
+	rank *ranking = (rank*)malloc(sizeof(rank)); //랭킹을 읽어올 구조체를 동적할당
+
+	list = ranking;
+
+	int stage = 1;
 	f1 = fopen("rank.def","r");
 
+	printf("stage : %d\n",stage);    // map.h파일을 불러오면 while(stage <= Maxstage)를 사용하여 코드 크기를 줄여야함
 	printf("name\tmove count\n\n");
-	while(!feof(f1)){
-		fscanf(f1,"%s %d",ranking->name,&ranking->move_count);
-		printf("%s\t%d",ranking->name,ranking->move_count);
+	fscanf(f1,"%d %s %d",ranking->stage,ranking->name,&ranking->move_count);
+
+	while(!feof(f1)&&stage==1){
+		if(stage != ranking->stage){
+			stage++;
+			break;
+		}
+		printf("%s\t%d",ranking->stage,ranking->name,ranking->move_count);
 		ranking = ranking->next;
+		fscanf(f1,"%d %s %d",ranking->stage,ranking->name,&ranking->move_count);
 	}
+
+	printf("stage : %d\n",stage);
+	printf("name\tmove count\n\n");
+	fscanf(f1,"%d %s %d",ranking->stage,ranking->name,&ranking->move_count);
+
+	while(!feof(f1)&&stage==2){
+		if(stage != ranking->stage){
+			stage++;
+			break;
+		}
+		printf("%s\t%d",ranking->stage,ranking->name,ranking->move_count);
+		ranking = ranking->next;
+		fscanf(f1,"%d %s %d",ranking->stage,ranking->name,&ranking->move_count);
+	}
+
 	system("pause");
 	system("cls");
+
+	free_rank(list);
 	fclose(f1);
 	return;
+}
+
+void free_rank(rank *list){
+	if(list->next != NULL){
+		free(list);
+		return;
+	}
 }
