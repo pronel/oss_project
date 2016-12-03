7d484def9e896f5214c5ccb5e00d4bff796a8e87
DESKTOP-V6J90SR\박민식 <alstlr448@naver.com>
2016-12-03 19:29:44 +0900

    원하는 스테이지기록 가져오기

--- rank.h
+++ rank.h
@@ -1,60 +1,56 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "map.h"
-
-typedef struct RANK {
+#include<stdio.h>
+#include<stdlib.h>
+//노드 구조정의
+typedef struct ranking {
 	int stage;
-	char name[5];
+	char name[7];
 	int move_count;
-	struct RANK *next;
+	struct ranking *next;
 }rank;
-void print_rank() {
-	int select;
-	printf("스테이지를 입력하세요. : \n");
-	scanf("%d", select);
+
+
+
+int main(void)
+{
+	int sum = 0;//스테이지를 입력받는 변수
+	printf("원하는 스테이지를 입력하세요\n");
+	scanf("%d", &sum);
 
 	FILE *f1;
-	rank *list, *ptr, *str;//기록을 찾는 노드
-	rank *compare, *temp, *node;//기록들을 비교하는 노드
-	list = (rank*)malloc(sizeof(rank));
-	compare = (rank*)malloc(sizeof(rank));
-	ptr = str = list;
-	temp = node = compare;
 	f1 = fopen("rank.txt", "r");
-	if (f1 == NULL)//파일의 유무를 확인
-	{
-		printf("error\n");
-	}
+	rank *list, *head,
+		*current, *fallow;
+	list = current = fallow = (rank*)malloc(sizeof(rank));
 
-	printf("stage : %d\n", select);
-	printf("name\t move_count\n\n");
+	while (!feof(f1)) { //파일안에 있는정보를 노드에 저장
+		fscanf(f1, "%d %s %d", &current->stage, current->name, &current->move_count);
+		if (sum == current->stage)//원하는 스테이지의 기록을 노드에 저장
+		{
+			head = (rank*)malloc(sizeof(rank));
 
-	while (!feof(f1))
-	{
-		fscanf(f1, "%d %s %d", list->stage, list->name, list->move_count);
+			fallow = current;
+			current->next = head;
+			current = current->next;
+		}
+		else
+		{
+			printf("없는 스테이지입니다\n");
+			continue;
+		}
+
 	}
-	while (list != NULL)//원하는 데이터를 cmpare에 저장
-	{
-		ptr = str;
+	fallow->next = NULL;//쓰레기값출력 방지
+
+	rank *str;//출력용 포인터 생성
+	str = list;
+	while (str != NULL) { //노드에 있는 정보출력 
+		printf("%d %s %d\n", str->stage, str->name, str->move_count);
 		str = str->next;
-		if (select == str->stage)
-		{
-			compare->stage = str->stage;
-			strcpy(compare->name, str->name);
-			compare->move_count = str->move_count;
-		}
 	}
 
-	while (compare != NULL)
-	{
-		temp = node;
-		node = node->next;
+	fclose(f1);
+	free(list);
 
-		if (temp->move_count > node->move_count)
-		{
-			temp = node->next;
-			node->next = temp;
-		}
-	}
+
 }
+
