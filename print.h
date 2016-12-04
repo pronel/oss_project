955e2b8a572e0ed8b0e4d1a735f1ef2bb81267d8
pronel <yc9870@naver.com>
2016-12-04 21:19:16 +0900

    Update print.h

--- print.h
+++ print.h
@@ -1,7 +1,43 @@
 #include"map.h"
 #ifndef PRINT_H
 #define PRINT_H
+void print(stage *st#ifndef PRINT_H 
+#define PRINT_H
+
+#include"map.h"
+
 void print(stage *st,int move_count) {
+	int x,y;
+	printf("───────────────────────────\n");
+	for (y = 0; y < 10; y++) {
+		for (x = 0; x < 35; x++) {
+			if (st->map[y][x] == '#') {
+				printf("■");			
+			}
+			else if (st->map[y][x] == ' ') {
+				printf("  ");
+			}else if (st->map[y][x] == '$') {
+				printf("○");			
+			}
+			else if (st->map[y][x] == '*') {
+				printf("★");
+			}else if (st->map[y][x] == 'X') {
+				printf("◎");
+			}else if (st->map[y][x] == 'o') {
+				printf("●");
+			}
+		}
+		printf("\n");
+	}
+	printf("───────────────────────────\n");
+	printf("───────────────────────────\n");
+	printf("\n");
+	printf("  이동횟수 : %d\n\n", move_count);
+
+	printf("  ○ : Stone   ★ : Player   ● : Hole");
+}
+
+#endif,int move_count) {
 	int x,y;
 	for (y = 0; y < 10; y++) {
 		for (x = 0; x < 35; x++) {
